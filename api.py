0cf3c98ee7d654cf1c0f9fc2b7ffcf0fa172a237
MCvD <maartenvandijk49@msn.com>
2016-10-25 23:48:31 +0200

    api code

---
+++ api.py
@@ -0,0 +1,23 @@
+import requests
+import xmltodict
+
+
+api_url = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=3yakjh3yeghyppmgt99azqkfjjfrundg&dag=25-10-2016&sorteer=0'
+response = requests.get(api_url)
+
+
+filmsXML = xmltodict.parse(response.text)
+
+for films in filmsXML['filmsoptv']['film']:
+    film = films['titel']
+    jaar = films['jaar']
+    duur = films['duur']
+    genre = films['genre']
+    synopsis = films['synopsis']
+    regisseur = films['regisseur']
+    cast = films['cast']
+    land = films['land']
+    cover = films['cover']
+
+
+    print(film + ' ' + jaar)
