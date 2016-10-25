ee4ec41c5ced8ec14b6e87e7acfa0cb5ab12aa1e
donaldhioe <donald061098@hotmail.com>
2016-10-25 22:15:19 +0200

    Add files via upload
    ik heb het handmatig gestuurd

--- startmenu.py
+++ startmenu.py
@@ -1,28 +1,52 @@
 from tkinter import *
 def startmenu():
-    root = Tk()
+    root.withdraw()
 
-    label = Label(master=root, text='hoofdmenu')
+    def volgende():
+        window2.withdraw()
+    window2 = Toplevel(master=root)
+    label = Label(master=window2, text='ticket:')
     label.pack()
 
-    label = Label(master=root,text='naam',height=4)
+    label = Label(master=window2, text=entry.get())
     label.pack()
 
-    entry = Entry(master=root)
-    entry.pack(padx=10, pady=10)
-
-    label = Label(master=root,text='mailadres',height=6)
+    label = Label(master=window2, text=entry2.get())
     label.pack()
 
-    entry = Entry(master=root)
-    entry.pack(padx=10, pady=10)
+    label = Label(master=window2, text='film:')
+    label.pack()
 
-    button = Button(master=root, text='Submit', command='')
-    button.pack(pady=10)
+    button1 = Button(master=window2, text='afsluiten', command=volgende)
+    button1.pack(padx=10, pady=10)
 
+root = Tk()
 
-    root.mainloop()
+label = Label(master=root, text='startmenu')
+label.pack()
+
+label = Label(master=root,text='naam')
+label.pack()
+
+entry = Entry(master=root)
+entry.pack(padx=10, pady=10)
+
+label = Label(master=root,text='mailadres')
+label.pack()
+
+entry2 = Entry(master=root)
+entry2.pack(padx=10, pady=10)
+
+label = Label(master=root,text='opties aanbieder')
+label.pack()
+
+label = Label(master=root,text='opties voor films')
+label.pack()
+
+button1 = Button(master=root, text='Submit', command=startmenu)
+button1.pack(pady=10)
+
+root.mainloop()
 
 startmenu()
 
-print('test')
