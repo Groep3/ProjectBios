778361c716587d511ca8cc60670a6561f5d019e8
FlorianKorzelius <fkorzelius@live.nl>
2016-10-26 16:49:55 +0200

    u

--- qrcode.py
+++ qrcode.py
@@ -6,7 +6,7 @@ def createqr():
     aanmeldcode = random.randrange(100000, 1000000)
     qrcode = pyqrcode.create('{}'.format(aanmeldcode), error='L', version=10)
     qrcode.png('qrcode.png', scale=5)
-    print(aanmeldcode)
-    qrcode.show()
+    return(aanmeldcode)
+    #qrcode.show()
 
 createqr()
