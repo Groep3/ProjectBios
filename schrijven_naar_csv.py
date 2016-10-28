e2730ba620053940b825397aecce87a8b3159705
FlorianKorzelius <fkorzelius@live.nl>
2016-10-28 12:57:56 +0200

    u

---
+++ schrijven_naar_csv.py
@@ -0,0 +1,36 @@
+import csv
+def aanbiedingMaarten():
+    with open('aanbiedingMaarten'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+
+def gekozenaanbiedingenMaarten():
+    with open('gekozenaanbiedingenMaarten'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+
+def aanbiedingFlorian():
+    with open('aanbiedingFlorian'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+def gekozenaanbiedingenFlorian():
+    with open('gekozenaanbiedingenFlorian'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+
+def aanbiedingDonald():
+    with open('aanbiedingDonald'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+def gekozenaanbiedingenDonald():
+    with open('gekozenaanbiedingenDonald'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+
+def aanbiedingLiza():
+    with open('aanbiedingLiza'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+def gekozenaanbiedingenLiza():
+    with open('gekozenaanbiedingenLiza'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+
+def aanbiedingJody():
+    with open('aanbiedingJody'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
+def gekozenaanbiedingenJody():
+    with open('gekozenaanbiedingenJody'+' '+date+'.csv', newline='') as f:
+        writer = csv.writer(f, delimiter='')
